9f6498701c8d86e5a2c2cbda38140b024e566411
Amir Yasaei <amiru@Amiru.local>
2025-02-20 16:22:14 -0500

    made search bar as input

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import "./index.css";
 import navSearchIcon from '../../assets/img/navSearchIcon.svg';
+import Input from "@mui/material/Input";
+import { useState } from 'react';
 
-export default function SearchBar({isNavbar}) {
+export default function SearchBar({isNavbar, placeholder}) {
+    const [input, setInput] = useState("")
     return (
         <>
         {isNavbar ? 
@@ -12,7 +15,13 @@ export default function SearchBar({isNavbar}) {
             </div>
         :
             <div className='titleSearch'>
-                Search for a residence or address ...
+                <Input 
+                    className='input-wrapper'
+                    placeholder='Search for a residence or address ...'
+                    disableUnderline={true}
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
                 <img src={navSearchIcon}/>
             </div>
         }
